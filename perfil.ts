ec5ce87a7fb3e229d4b2c67012448e449754eaf6
Morgan <morgan.espindola@gmail.com>
2024-02-22 07:43:35 -0300

    First examples

---
+++ perfil.ts
@@ -0,0 +1,8 @@
+let Morgan = {
+  fullName: "Morgan Espindola",
+  since: 1989,
+  devSince: 2018,
+  nttSince: 2022,
+  hobbies: ["Games", "TV Series", "Cooking"],
+  mainStack: "Angular",
+};
---
+++ variables/001-variable.ts
@@ -0,0 +1,5 @@
+// Mostre no console o nome de uma pessoa, a data de fechamento e a data de vencimento do cartão de crédito
+
+const a = 10;
+const b = 5;
+const n = "John";
