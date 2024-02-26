fc7dd2bf45056d2ef3344234e4ac9be078b91c12
Morgan <morgan.espindola@gmail.com>
2024-02-26 08:26:30 -0300

    Updated 001 variable

--- variables/001-variable.ts
+++ variables/001-variable.ts
@@ -1,5 +1,10 @@
 // Mostre no console o nome de uma pessoa, a data de fechamento e a data de vencimento do cartão de crédito
+// Melhore o código usando Código Limpo
 
 const a = 10;
 const b = 5;
 const n = "John";
+
+console.log(
+  `Olá, ${n}.A Fatura do seu cartão de crédito fecha dia ${b} e vence dia ${a}`
+);
