9d1ef753f96a977fa6b4fec60d88a5998316fccf
Morgan <morgan.espindola@gmail.com>
2024-02-26 08:29:46 -0300

    Created class 001 model

---
+++ classes/001-class.ts
@@ -0,0 +1,15 @@
+// Melhore as classes usando código limpo
+
+class Carro {
+  cor: string;
+  marca: string;
+  modelo: string;
+  quantidadePortas: number;
+}
+
+class Motocicleta {
+  cor: string;
+  marca: string;
+  modelo: string;
+  modeloGuidao: string;
+}
