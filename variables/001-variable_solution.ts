614edc3027b9f7d410c8c900dcbec2881aa6afe8
Morgan <morgan.espindola@gmail.com>
2024-02-27 07:16:12 -0300

    Solution of variable 001

---
+++ variables/001-variable_solution.ts
@@ -0,0 +1,7 @@
+const diaVencimento = 10;
+const diaFechamento = 5;
+const nome = "John";
+
+console.log(
+  `Olá, ${nome}.A Fatura do seu cartão de crédito fecha dia ${diaFechamento} e vence dia ${diaVencimento}`
+);
